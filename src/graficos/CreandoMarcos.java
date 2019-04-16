e4b1c71501cea32750d782358335740654246df2
nestor2502 <nestor2502@ciencias.unam.mx>
2019-04-15 23:35:18 -0500

    se agrega clase

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -1,9 +1,26 @@
 package graficos;
+import javax.swing.*;
 
 public class CreandoMarcos{
 
 
-	public static void public static void main(String[] args) {
+	 public static void main(String[] args) {
+
+	 	miMarco marco1 = new miMarco();
+
+	 	marco1.setVisible(true);//metodo que hace visible la ventana
+
+	 	marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//metodo que termina el programa cuando se cierra la ventana
 		
+	}
+}
+
+class miMarco extends JFrame{
+
+	 public miMarco(){
+         
+         setSize(500, 300);
+         
+
 	}
 }
