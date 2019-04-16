7fe961ca7900f03e5583bcbfcfbd13fb74a4d0ef
nestor2502 <nestor2502@ciencias.unam.mx>
2019-04-16 02:18:57 -0500

    se agrega evento

---
+++ src/eventos/PruebaEventos.java
@@ -0,0 +1,70 @@
+package eventos;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class PruebaEventos{
+     
+     public static void main(String[] args) {
+     	
+     	MarcoBotones mimarco = new MarcoBotones();
+
+     	mimarco.setVisible(true);
+
+     	mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
+
+
+}
+
+class MarcoBotones extends JFrame{
+
+
+	  public MarcoBotones(){
+
+	  	   setTitle("Botones y Eventos");
+
+	  	   setBounds(500, 300, 500, 300);
+
+	  	   LaminaBotones milamina = new LaminaBotones();
+
+	  	   add(milamina);
+	  }
+}
+
+class LaminaBotones extends JPanel implements ActionListener{
+
+
+	 JButton botonAzul = new JButton("Azul");
+	 JButton botonAmarillo = new JButton("Amarillo");
+	 JButton botonRojo = new JButton("Rojo");
+
+	 public LaminaBotones(){
+
+		  add(botonAzul);
+		  add(botonAmarillo);
+		  add(botonRojo);
+          //Action es lo mismo que decirle: al hacer click (Objeto evento)
+          //botonAzul (Objeto fuente)
+          //this (Objeto fuente)
+		  botonAzul.addActionListener(this);
+		  botonRojo.addActionListener(this);
+		  botonAmarillo.addActionListener(this);
+	}
+
+    @Override
+	public void actionPerformed(ActionEvent e){
+         //capturar el origen del evento
+		 Object botonPulsado = e.getSource();//e es el objeto evento 
+
+		 if(botonPulsado == botonAzul)
+		 setBackground(Color.blue);
+
+		 else if(botonPulsado == botonAmarillo)
+		 setBackground(Color.yellow);
+
+		 else
+		 setBackground(Color.red);
+	}
+} 
